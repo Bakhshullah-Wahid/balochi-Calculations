824d716380083a912228df11e19a28847c5df229
bakhshullah wahid <bakhshullahwahid205@gmail.com>
2023-09-15 15:13:07 +0500

    adding the values with the yak till beesth

--- toBuild/functions.py
+++ toBuild/functions.py
@@ -1,47 +1,58 @@
-# importing basic data dictionaries that are used in this function
 import keywords as k
-# function to do arethmetic operation
-def toOperate(value , value2 , value3):
-    if(value2 == '+'):
-        v=(value + value3)
-        if(v>20):
-            print('work is on progress')
-        else:
-            return  v
-    elif(value2 == '-'):
-        v=(value - value3)
-        if(v>20 or v <0 ):
-            print('work is on progress')
-        else:
-            return  v
-    elif (value2 == '*'):
-        v=(value * value3)
-        if(v>20 or v<0):
-            print('work is on progress')
-        else:
-            return  v
-    elif(value2 == '/'):
-        v=(value / value3)
-        if((value / value3).float):
-            print('work progress')
+import separation as s
 
-#giving values for arethemetics operation
-#spilit functions is to spilit the string according o the spaces
-input = "PUNCH ZARBE CHAAR" .split()
-# token is used to store each value in the form of the list
-token = []
-# for loop is used to tokenize or make lexemes of each input given
-for word in input:
-    # checks if the input contain sth that belongs or is number
+# function to do arithmetic operations
+def toOperate(value, value2, value3):
+    v = None
+    digit = []
+    if value2 == '+':
+        v = value + value3
+        if v > 20:
+            digit.append(v // 10)
+            digit.append(v % 10)
+           
+            return digit
+        else:
+            digit.append(v)
+            return digit
+    elif value2 == '-':
+        v = value - value3
+        if v > 20:
+            digit.append(v // 10)
+            digit.append(v % 10)
+           
+            return digit
+    elif value2 == '*':
+        v = value * value3
+       
+        if v > 20:
+            # digit.append(v // 10)
+            # digit.append(v % 10)
+            digit = s.change(v)
+           
+            return digit
+        else:
+            digit.append(v)
+            return digit
+
+input_string = "BEESTH JAMA BEESTH".split()
+tokens = []
+for word in input_string:
     if word in k.NUMBERS:
-        token.append(['NUMBERS' , k.NUMBERS[word]])
-    # checks if the input contain sth that belongs or is arethmetic
+        tokens.append(['NUMBERS', k.NUMBERS[word]])
     elif word in k.ARETHMATIC:
-        token.append(['OPERATOR' ,k.ARETHMATIC[word]])
-# printing the token
-print(token)
-# in this loop we are calling a function to do appropriate operations on behalf of the input
-for word in k.NUMBERS:
-    if(toOperate(token[0][1] , token[1][1] ,token[2][1] ) == k.NUMBERS[word]):
-        # at here we are just printing the value of the word or the k.numbers key value
-        print(word)
+        tokens.append(['OPERATOR', k.ARETHMATIC[word]])
+
+print(tokens)
+
+result_list = toOperate(tokens[0][1], tokens[1][1], tokens[2][1])
+print(result_list)
+
+result = ''
+
+for num in result_list:
+    for key, value in k.NUMBERS.items():
+        if value == num:
+            result += key
+
+print(result)
---
+++ toBuild/separation.py
@@ -0,0 +1,5 @@
+number = 300
+def change(number):
+    number_str = str(number)
+    digits = [int(digit) for digit in number_str]
+    return digits
